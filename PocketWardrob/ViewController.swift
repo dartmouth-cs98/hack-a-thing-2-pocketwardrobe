a7aa61cc5983916ba11715772fcc57a35b8cd8d8
Jane Lee <jane.lee.19@dartmouth.edu>
2019-01-16 21:19:28 -0500

    fifth times the charm

---
+++ PocketWardrob/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  PocketWardrobe
+//
+//  Created by Jane Lee on 1/15/19.
+//  Copyright © 2019 Jane Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var clothesNameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        clothesNameLabel.text = textField.text
+    }
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        //        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+        //            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        //        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        clothesNameLabel.text = "Default Text"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
